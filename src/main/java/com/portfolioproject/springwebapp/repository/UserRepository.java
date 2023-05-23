96962f762c301b1c9bdddc1195230fffef759ddf
Joshua Michael Du Plessis <joshuamichaelduplessis@Joshuas-MacBook-Pro.local>
2023-05-23 15:56:01 +0200

    initial

---
+++ src/main/java/com/portfolioproject/springwebapp/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.portfolioproject.springwebapp.repository;
+
+
+import com.portfolioproject.springwebapp.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
